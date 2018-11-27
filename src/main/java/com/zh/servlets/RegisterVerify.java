d9663f6381d1e5807089b1ca6be968bc079aba61
zhangheng <zhanghengscnc@msn.com>
2018-11-27 23:03:00 +0800

    Initial commit

---
+++ src/main/java/com/zh/servlets/RegisterVerify.java
@@ -0,0 +1,35 @@
+package com.zh.servlets;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author ZH
+ * @create 2018-11-27 22:46
+ */
+public class RegisterVerify implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!5522333llllllllpppppp</h2>
+</body>
+</html>
